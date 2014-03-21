f41b5cf4f2bf39d2877f7526156ef1f62c77b1a3
Juliano Alves <von.juliano@gmail.com>
2014-03-21 19:01:04 -0300

    first features

---
+++ Ioget.Tests/IogetTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Ioget.Tests
+{
+    [TestFixture]
+    public class IogetTest
+    {
+        [Test]
+        public void ShouldContructObjectWithParamenterString()
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"f1", "a"},
+            };
+
+            var myObject = new Instantiator().Bind(dic, typeof(MyString));
+            Assert.AreEqual(new MyString("a"), myObject);
+        }
+
+        [Test]
+        public void ShouldContructObjectWithParameterInt()
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"f1", "1"}
+            };
+
+            var myObject = new Instantiator().Bind(dic, typeof(MyInt));
+            Assert.AreEqual(new MyInt(1), myObject);
+        }
+
+        [Test]
+        public void ShouldWorkWithTuples()
+        {
+            var dic = new Dictionary<string, string>
+            {
+                {"item1", "a"}
+            };
+
+            var myObject = new Instantiator().Bind(dic, typeof(Tuple<string>));
+            Assert.AreEqual(new Tuple<string>("a"), myObject);
+        }
+    }
+
+    public struct MyString
+    {
+        private readonly string f1;
+
+        public MyString(string f1)
+        {
+            this.f1 = f1;
+        }
+    }
+
+    public struct MyInt
+    {
+        private readonly int f1;
+
+        public MyInt(int f1)
+        {
+            this.f1 = f1;
+        }
+    }
+}
---
+++ Ioget/Ioget.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Ioget
+{
+
+    public interface Conversion
+    {
+        bool IsApplicable(ParameterInfo info);
+        object Apply(string value);
+    }
+
+
+    public class StringConversion : Conversion
+    {
+        public bool IsApplicable(ParameterInfo value)
+        {
+            return true;
+        }
+
+        public object Apply(string value)
+        {
+            return value;
+        }
+    }
+
+    public class Instantiator
+    {
+        private readonly List<Conversion> convertions; 
+        public Instantiator()
+        {
+            this.convertions = new List<Conversion>
+            {
+                new IntConvertion(), new StringConversion()
+            };
+        }
+
+        public object Bind(Dictionary<string, string> dic, Type type)
+        {
+            var constructor = type.GetConstructors().First();
+            var parameters = constructor.GetParameters().Select(p => ResolveType(p, dic[p.Name]));
+            return constructor.Invoke(parameters.ToArray());
+        }
+
+        private object ResolveType(ParameterInfo info, string value)
+        {
+            return convertions.First(c => c.IsApplicable(info)).Apply(value);
+        }
+    }
+
+    public class IntConvertion : Conversion
+    {
+
+        public bool IsApplicable(ParameterInfo info)
+        {
+            return info.ParameterType == typeof(int);
+        }
+
+        public object Apply(string value)
+        {
+            return Convert.ToInt32(value);
+        }
+    }
+}
